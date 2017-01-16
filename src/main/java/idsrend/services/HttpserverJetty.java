c79aeb06ea146580fcd59fb3813adde585ac793e
shangkuanlc <shangkuanlc@gmail.com>
2017-01-16 13:05:25 +0800

    翻譯第二版
    統一翻譯用註解風格 https://wikimedians-tw.hackpad.com/-han3_ji7_tsoo1_kian3-kYUXw8BAWRH#:h=翻譯用註解風格
    的結果

--- src/main/java/idsrend/services/HttpserverJetty.java
+++ src/main/java/idsrend/services/HttpserverJetty.java
@@ -8,16 +8,20 @@ import org.eclipse.jetty.servlet.ServletHandler;
  *
  * @author Ihc
  */
-/**
+/*e
  * I use Jetty as the mainframe end, to provide the service for ideograph sequencing.
  *
  * @author Ihc
  */
 public class HttpserverJetty
 {
-	/** 網路監聽埠口 */
-	/** internet monitor port */	
-	static final int 連接埠 = 8060;//連接埠 means connection port
+	/**
+	 * 網路監聽埠口
+	 */
+	/*e
+	 * internet monitor port
+	 */
+	static final int 連接埠 = 8060; //e 連接埠 means connection port
 
 	/**
 	 * 主要執行的函式。
@@ -25,26 +29,26 @@ public class HttpserverJetty
 	 * @param 參數
 	 *            無使用著。
 	 */
-	/**
+	/*e
 	 * Functions that execute the most
 	 *
 	 * @param parameter
 	 *            Not used.
 	 */
-	public static void main(String[] 參數)//參數 means parameter
+	public static void main(String[] 參數) //e 參數 means parameter
 	{
 		Server server = new Server(8060);
 
 		ServletHandler handler = new ServletHandler();
 		server.setHandler(handler);
 		handler.addServletWithMapping(IDSrendServlet.class, "/*");
-		boolean 遏袂啟動 = true;//遏袂啟動 means Not activated yet
-		while (遏袂啟動)//遏袂啟動 means Not activated yet
+		boolean 遏袂啟動 = true; //e 遏袂啟動 means Not activated yet
+		while (遏袂啟動) //e 遏袂啟動 means Not activated yet
 		{
 			try
 			{
 				server.start();
-				遏袂啟動 = false;//遏袂啟動 means Not activated yet
+				遏袂啟動 = false; //e 遏袂啟動 means Not activated yet
 			}
 			catch (Exception e)
 			{
@@ -58,7 +62,7 @@ public class HttpserverJetty
 				}
 			}
 		}
-		System.out.println("服務啟動～～");//服務啟動～～ means Service activate
+		System.out.println("服務啟動～～"); //e 服務啟動～～ means Service activate~~
 		try
 		{
 			server.join();
