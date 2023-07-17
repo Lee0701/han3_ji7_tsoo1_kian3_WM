bd1c548cf60002cdedb2c239e8ab77155e9b94af
Lee0701 <hgl1002@naver.com>
2023-07-17 14:59:47 +0900

    Update dependencies

--- src/main/java/idsrend/services/HttpserverJetty.java
+++ src/main/java/idsrend/services/HttpserverJetty.java
@@ -25,7 +25,7 @@ public class HttpserverJetty
 
 		ServletHandler handler = new ServletHandler();
 		server.setHandler(handler);
-		handler.addServletWithMapping(IDSrendServlet.class, "/*");
+		handler.addServletWithMapping(IDSrendServlet.class.getName(), "/*");
 		boolean 遏袂啟動 = true;
 		while (遏袂啟動)
 		{
